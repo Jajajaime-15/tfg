3303e0274e0fa0e5ddc64e4ee28bd30b1711899d
Jlfl95 <juliofloresmartin2011@gmail.com>
2026-03-18 11:01:47 +0100

    features: implementacion ventana login

--- proyecto_flet/src/main.py
+++ proyecto_flet/src/main.py
@@ -1,25 +1,50 @@
 import flet as ft
+from flet import Checkbox, FloatingActionButton, Icons, Page, TextField
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.title = "Login"
+    cabecera = ft.Text("LOGIN", size=40, color=ft.Colors.WHITE)
+    nombre = TextField(hint_text="Nombre", width=250,color = ft.Colors.BLACK, bgcolor=ft.Colors.WHITE)
+    password = TextField(hint_text="Password", width=250, password=True,bgcolor=ft.Colors.WHITE)
+    boton_login = ft.ElevatedButton("LOGIN", width=150, bgcolor=ft.Colors.BLACK)
     page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
-        )
+    ft.Container(
+        width=400,
+        height=500,
+        bgcolor=ft.Colors.BLUE,
+        border_radius=ft.BorderRadius.all(20),
+        padding=20,
+        content=ft.Column(
+            controls=[
+                ft.Container(
+                    expand=1,  # Espaciador superior (2 partes)
+                ),
+                ft.Row(controls=[cabecera], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Container(
+                    expand=2,  # Espaciador superior (2 partes)
+                ),
+                ft.Row(controls=[nombre], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Row(controls=[password], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Container(
+                    expand=1,  # Espaciador inferior (8 partes)
+                ),
+                ft.Row(
+                            controls=[boton_login],
+                            alignment=ft.MainAxisAlignment.CENTER
+                        ),
+                ft.Container(
+                    expand=8,  # Espaciador inferior (8 partes)
+                ),
+            ]
+        ),
+        
+        
     )
+)
 
 
-ft.run(main)
+if __name__ == "__main__":
+    ft.run(main)
