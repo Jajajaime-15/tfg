b3c47360eeba24b03bd62078b177b12d26c22bb5
Jlfl95 <juliofloresmartin2011@gmail.com>
2026-03-24 12:38:09 +0100

    refactor: Creacion directorio views con login y registro. features: implementacion navegacion entre login y registro

--- proyecto_flet/src/main.py
+++ proyecto_flet/src/main.py
@@ -1,102 +1,16 @@
 import flet as ft
-from flet import Checkbox, FloatingActionButton, Icons, Page, TextField
-from components.components import PrimaryButton, SecondaryButton, IconButton
 from database.wrapper import Wrapper
 from controllers.auth_controller import AuthController
 from router import Router
-
 
 async def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.bgcolor = ft.Colors.with_opacity(0.95, ft.Colors.BLUE_GREY_100)
     page.title = "Login"
-    cabecera = ft.Text(
-    "¡Bienvenido!", 
-    size=40,
-    color=ft.Colors.WHITE,
-    weight=ft.FontWeight.BOLD,
-    )
-    nombre = TextField(
-        hint_text="Nombre", 
-        width=250,
-        color = ft.Colors.BLACK, 
-        bgcolor=ft.Colors.WHITE,
-        prefix_icon=ft.CupertinoIcons.PROFILE_CIRCLED,
-        border_radius=20,
-        autofocus = True)
-    password = TextField(
-        hint_text="Password", 
-        width=250, 
-        password=True,
-        color=ft.Colors.BLACK,
-        bgcolor=ft.Colors.WHITE,
-        prefix_icon = ft.CupertinoIcons.LOCK,
-        can_reveal_password=True,
-        border_radius=20,
-        )
-    boton_login = ft.ElevatedButton(
-        "LOGIN", 
-        width=150,
-        style=ft.ButtonStyle(
-            text_style=ft.TextStyle(
-                weight=ft.FontWeight.BOLD,
-                size=16
-            ),
-            color={
-                    ft.ControlState.HOVERED: ft.Colors.BLACK,
-                    ft.ControlState.FOCUSED: ft.Colors.BLACK,
-                    ft.ControlState.DEFAULT: ft.Colors.BLACK,
-                },
-            bgcolor={
-                    ft.ControlState.HOVERED: ft.Colors.GREY_200,
-                    ft.ControlState.FOCUSED: ft.Colors.GREY_200,
-                    ft.ControlState.DEFAULT: ft.Colors.WHITE,
-                },    
-        ))
-    page.add(
-    ft.Container(
-        width=400,
-        height=500,
-        bgcolor=ft.Colors.BLUE,
-        border_radius=ft.BorderRadius.all(20),
-        padding=20,
-        shadow=ft.BoxShadow(
-            blur_radius=15,
-            color=ft.Colors.with_opacity(0.3, ft.Colors.BLACK), 
-            offset=ft.Offset(3, 3)
-        ),
-        content=ft.Column(
-            controls=[
-                ft.Container(
-                    expand=1,  
-                ),
-                ft.Row(controls=[cabecera], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Container(
-                    expand=2,  
-                ),
-                ft.Row(controls=[nombre], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Row(controls=[password], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Container(
-                    expand=8,  
-                ),
-                ft.Row(
-                            controls=[PrimaryButton("LOGIN", width=200)],
-                            alignment=ft.MainAxisAlignment.CENTER
-                        ),
-                ft.Row(
-                            controls=[SecondaryButton("SIGN UP", width=200)],
-                            alignment=ft.MainAxisAlignment.CENTER
-                        ),
-                ft.Container(
-                    expand=8,  
-                ),
-            ]
-        ),
-        
-        
-    )
-    )
+    page.window_width = 400
+    page.window_height = 700
+    
     # Inicializamos la base de datos y el controlador
     wrapper = Wrapper(page)
     controlador_auth = AuthController(page, wrapper)
@@ -111,7 +25,3 @@ async def main(page: ft.Page):
     await my_router.route_change(None)
 
 ft.app(target=main)
-
-
-if __name__ == "__main__":
-    ft.run(main)
