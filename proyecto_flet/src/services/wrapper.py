c2e0ae2efcef937ec5ef459e104124972ce109df
Alba <barrosomartinalba@gmail.com>
2026-04-11 15:50:00 +0200

    Service: wrapper modificado añadiendo las funciones correspondientes a cambiar contraseña, borrar cuenta

--- proyecto_flet/src/services/wrapper.py
+++ proyecto_flet/src/services/wrapper.py
@@ -160,3 +160,45 @@ class Wrapper:
         except Exception as e:
             print(f"Error: {e}")
             return False, str(e)
+
+    # funcion para cambiar la contraseña estando conectado    
+    async def cambiar_psw(self,nueva_psw):
+        try:
+            # obtenemos el token si no está en memoria
+            if not self.token:
+                self.token = await self.page.shared_preferences.get("token")
+            
+            # actualizamos la contraseña
+            self.auth.update_user_password(self.token,nueva_psw)
+            print("Contraseña actualizada")
+            return True, "Contraseña actualizada"
+        except Exception as e:
+            # si el token está caducado, refrescamos el token
+            if await self.actualizar_sesion():
+                try:
+                    self.auth.update_user_password(self.token,nueva_psw)
+                    return True, "Contraseña actualizada"
+                except Exception as error:
+                    return False, str(error)
+            return False, "Sesión caducada, vuelve a iniciar"
+        
+    # funcion para eliminar la cuenta y los datos de dicha cuenta
+    async def borrar_cuenta(self):
+        try:
+            if not self.id_usuario:
+                self.id_usuario = await self.page.shared_preferences.get("id_usuario")
+            if not self.token:
+                self.token = await self.page.shared_preferences.get("token")
+
+            # borramos toda la informacion de la base de datos (de Realtime)
+            self.db.child("usuarios").child(self.id_usuario).remove(self.token)
+            # borramos el usuario de Authentication
+            self.auth.delete_user_account(self.token)
+            # una vez eliminado cerramos sesión
+            await self.cerrar_sesion()
+
+            print("Cuenta eliminada")
+            return True, "La cuenta ha sido eliminada"
+        except Exception as e:
+            print(f"Error al borrar la cuenta: {e}")
+            return False, "Error al eliminar cuenta, inténtalo de nuevo iniciando sesión"
