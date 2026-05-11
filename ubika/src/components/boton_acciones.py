3441a84e697814842bb9641bd6986ca6ac10223a
Alba <barrosomartinalba@gmail.com>
2026-05-11 14:57:06 +0200

    Components: creado un componente IconButton general para las acciones y eliminados agregar,editar y eliminar

---
+++ ubika/src/components/boton_acciones.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+# este botón es para cualquier accion que usemos que solo sea con icono (guardar, editar, cancelar...)
+class BotonAcciones(ft.IconButton):
+    def __init__(self, icon, accion, tooltip="", color_icono="#1A6AFE", size=20, ancho=40,  visible=True, deshabilitado=False):
+        super().__init__()
+        self.icon = icon
+        self.on_click = accion
+        self.tooltip = tooltip
+        self.icon_color = color_icono
+        self.icon_size = size
+        self.width = ancho
+        self.height = ancho
+        self.visible = visible
+        self.disabled = deshabilitado
+        self.style = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10), padding=0)
